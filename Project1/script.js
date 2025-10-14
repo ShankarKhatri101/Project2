0798a913bc019242f3f44eddabf804626ecbc504
shankarkhatri <khatrishankar101@gmail.com>
2025-10-14 15:07:35 +0530

    sec commit

--- Project1/script.js
+++ Project1/script.js
@@ -1,38 +1,38 @@
 
-// gsap.from("#img1",{
-//     y: 100,
-//     delay:1.5,
-//     opacity: 0,
-//     duration: 5,
-//     stagger:2
+gsap.from("#img1",{
+    y: 100,
+    delay:1.5,
+    opacity: 0,
+    duration: 5,
+    stagger:2
     
-// });
-// gsap.from("#img2",{
-//     y: 100,
-//     delay:1.5,
-//     opacity: 0,
-//     duration: 5
+});
+gsap.from("#img2",{
+    y: 100,
+    delay:1.5,
+    opacity: 0,
+    duration: 5
     
-// });
-// gsap.from("#img3",{
-//     y: 100,
-//     delay:1.5,
-//     opacity: 0,
-//     duration: 5
+});
+gsap.from("#img3",{
+    y: 100,
+    delay:1.5,
+    opacity: 0,
+    duration: 5
     
-// });gsap.from("#img4",{
-//     y: 100,
-//     delay:1.5,
-//     opacity: 0,
-//     duration: 5
+});gsap.from("#img4",{
+    y: 100,
+    delay:1.5,
+    opacity: 0,
+    duration: 5
     
-// });gsap.from("#img5",{
-//     y: 100,
-//     delay:1.5,
-//     opacity: 0,
-//     duration: 5,
-//     stagger:2
-// });
+});gsap.from("#img5",{
+    y: 100,
+    delay:1.5,
+    opacity: 0,
+    duration: 5,
+    stagger:2
+});
 
 gsap.from("img",{
     y: 100,
